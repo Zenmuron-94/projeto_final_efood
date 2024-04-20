b7349b2d372dbcc5b0495a4161f659c5d2f68ae9
Clóvis Pujol <bioclp@hotmail.com>
2024-04-20 17:54:15 -0300

    Modal do prato e integrando API pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,86 +1,111 @@
 import Header from '../../components/Header'
 import ListaRestaurantes from '../../components/ListaRestaurantes'
-import ModeloRestaurante from '../../models/Restaurante'
+//import ModeloRestaurante from '../../models/Restaurante'
 
-import prato1 from '../../assets/images/imagem.png'
-import prato2 from '../../assets/images/image1.png'
-import estrela from '../../assets/images/estrela.png'
+//import prato1 from '../../assets/images/imagem.png'
+//import prato2 from '../../assets/images/image1.png'
+//import estrela from '../../assets/images/estrela.png'
 
-const comercios: ModeloRestaurante[] = [
-  {
-    id: 1,
-    infos: ['Destaque da semana', 'Japonesa'],
-    image: prato1,
-    title: 'Hioki Sushi',
-    number: '4.9',
-    star: estrela,
-    description:
-      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!',
-    // eslint-disable-next-line react/jsx-key
-    button: 'Saiba mais'
-  },
-  {
-    id: 2,
-    infos: ['Italiana'],
-    image: prato2,
-    title: 'La Dolce Vita Trattoria',
-    number: '4.6',
-    star: estrela,
-    description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    button: 'Saiba mais'
-  },
-  {
-    id: 3,
-    infos: ['Italiana'],
-    image: prato2,
-    title: 'La Dolce Vita Trattoria',
-    number: '4.6',
-    star: estrela,
-    description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    button: 'Saiba mais'
-  },
-  {
-    id: 4,
-    infos: ['Italiana'],
-    image: prato2,
-    title: 'La Dolce Vita Trattoria',
-    number: '4.6',
-    star: estrela,
-    description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    button: 'Saiba mais'
-  },
-  {
-    id: 5,
-    infos: ['Italiana'],
-    image: prato2,
-    title: 'La Dolce Vita Trattoria',
-    number: '4.6',
-    star: estrela,
-    description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    button: 'Saiba mais'
-  },
-  {
-    id: 6,
-    infos: ['Italiana'],
-    image: prato2,
-    title: 'La Dolce Vita Trattoria',
-    number: '4.6',
-    star: estrela,
-    description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    button: 'Saiba mais'
-  }
-]
+import { useEffect, useState } from 'react'
 
-const Home = () => (
-  <>
-    <Header />
-    <ListaRestaurantes comercios={comercios} />
-  </>
-)
+// const comercios: ModeloRestaurante[] = [
+//   {
+//     id: 1,
+//     infos: ['Destaque da semana', 'Japonesa'],
+//     image: prato1,
+//     title: 'Hioki Sushi',
+//     number: '4.9',
+//     star: estrela,
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, nihil suscipit nam libero, ipsum sit saepe inventore veritatis quis hic, assumenda molestias reprehenderit officiis natus earum cupiditate perspiciatis excepturi. Mollitia?',
+//     // eslint-disable-next-line react/jsx-key
+//     button: 'Saiba mais'
+//   },
+//   {
+//     id: 2,
+//     infos: ['Japonesa'],
+//     image: prato1,
+//     title: 'Hioki Sushi',
+//     number: '4.6',
+//     star: estrela,
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, nihil suscipit nam libero, ipsum sit saepe inventore veritatis quis hic, assumenda molestias reprehenderit officiis natus earum cupiditate perspiciatis excepturi. Mollitia?',
+//     button: 'Saiba mais'
+//   },
+//   {
+//     id: 3,
+//     infos: ['Japonesa'],
+//     image: prato1,
+//     title: 'Hioki Sushi',
+//     number: '4.6',
+//     star: estrela,
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, nihil suscipit nam libero, ipsum sit saepe inventore veritatis quis hic, assumenda molestias reprehenderit officiis natus earum cupiditate perspiciatis excepturi. Mollitia?',
+//     button: 'Saiba mais'
+//   },
+//   {
+//     id: 4,
+//     infos: ['Japonesa'],
+//     image: prato1,
+//     title: 'Hioki Sushi',
+//     number: '4.6',
+//     star: estrela,
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, nihil suscipit nam libero, ipsum sit saepe inventore veritatis quis hic, assumenda molestias reprehenderit officiis natus earum cupiditate perspiciatis excepturi. Mollitia?',
+//     button: 'Saiba mais'
+//   },
+//   {
+//     id: 5,
+//     infos: ['Japonesa'],
+//     image: prato1,
+//     title: 'Hioki Sushi',
+//     number: '4.6',
+//     star: estrela,
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, nihil suscipit nam libero, ipsum sit saepe inventore veritatis quis hic, assumenda molestias reprehenderit officiis natus earum cupiditate perspiciatis excepturi. Mollitia?',
+//     button: 'Saiba mais'
+//   },
+//   {
+//     id: 6,
+//     infos: ['Japonesa'],
+//     image: prato1,
+//     title: 'Hioki Sushi',
+//     number: '4.6',
+//     star: estrela,
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, nihil suscipit nam libero, ipsum sit saepe inventore veritatis quis hic, assumenda molestias reprehenderit officiis natus earum cupiditate perspiciatis excepturi. Mollitia?',
+//     button: 'Saiba mais'
+//   }
+// ]
+
+export type Restaurant = {
+  id: number
+  titulo: string
+  avaliacao: number
+  descricao: string
+  capa: string
+  button: string
+  destacado: boolean
+  tipo: string
+  cardapio?: string
+  preco?: number
+}
+
+const Home = () => {
+  const [restaurantes, setRestaurantes] = useState<Restaurant[]>([])
+
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/efood/restaurantes')
+      .then((resposta) => resposta.json())
+      .then((resposta) => setRestaurantes(resposta))
+  }, [])
+
+  return (
+    <>
+      <Header />
+      <ListaRestaurantes comercios={restaurantes} />
+    </>
+  )
+}
 
 export default Home
